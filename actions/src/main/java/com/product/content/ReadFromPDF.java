df402547d021865001b3b6e43b6de64aceb0d691
Anca Florin <florin.anca@microfocus.com>
2019-02-07 17:28:53 +0200

    added read from pdf

---
+++ actions/src/main/java/com/product/content/ReadFromPDF.java
@@ -0,0 +1,40 @@
+package com.product.content;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class ReadFromPDF {
+    public static void main(String[] args) throws IOException {
+
+        try (PDDocument document = PDDocument.load(new File("C:\\Users\\ancaf.CORPDOM\\OneDrive\\OneDrive - Micro Focus\\7 card\\PF639139.pdf"))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+                // split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+
+        }
+
+    }
+
+}
